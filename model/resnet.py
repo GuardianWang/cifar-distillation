cff5fee2f011880f23bbf387c4a85e1d7fbefb77
guardianwang <962247283@qq.com>
2022-05-29 03:03:18 -0700

    model

--- model/resnet.py
+++ model/resnet.py
@@ -1,21 +1,32 @@
 import torchvision.models as models
 from torch import nn, randn
+import torch
 
 
 class ResNet(nn.Module):
-    def __init__(self, n_out=100):
+    def __init__(self, cfg):
         super().__init__()
-        self.backbone = models.resnet50(pretrained=False)
-        self.fc = nn.Linear(self.backbone.fc.in_features, n_out)
-        self.backbone.fc = self.fc
+        self.cfg = cfg
+        resnet = models.resnet50(pretrained=False)
+        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
+        self.fc = nn.Linear(resnet.fc.in_features, cfg.classes)
 
     def forward(self, x):
-        return self.backbone(x)
+        x = self.backbone(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
 
 
 def test_resnet(in_shape=(2, 3, 32, 32)):
-    model = ResNet()
-    data = randn(in_shape)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = ResNet().to(device)
+    print(model)
+    model = model.train()
+    data = randn(in_shape).to(device)
+    o = model(data)
+    print(o.shape)
+    model = model.eval()
     o = model(data)
     print(o.shape)
 
