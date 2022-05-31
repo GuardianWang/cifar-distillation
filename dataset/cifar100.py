d158e6619154b2e983cb50418156c87357221a06
guardianwang <962247283@qq.com>
2022-05-31 02:49:58 -0700

    migrate test get data

--- dataset/cifar100.py
+++ dataset/cifar100.py
@@ -35,36 +35,5 @@ def get_classes(dataset):
     return dataset.classes
 
 
-def imshow(img):
-    img = img / 2 + 0.5  # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-
-def test_get_data(root=r".", batch_size=4):
-    def show_batch(loader):
-        plt.close('all')
-        # get some random training images
-        dataiter = iter(loader)
-        images, labels = dataiter.next()
-        print(f"image shape: {images.shape}")
-        print(f"labels shape: {labels.shape}")
-
-        # print labels
-        print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
-        # show images
-        imshow(torchvision.utils.make_grid(images))
-
-    train_dataset, train_loader = get_data(root, train=True, batch_size=batch_size)
-    test_dataset, test_loader = get_data(root, train=False, batch_size=batch_size)
-    classes = get_classes(train_dataset)
-
-    print("show train loader")
-    show_batch(train_loader)
-    print("show test loader")
-    show_batch(test_loader)
-
-
 if __name__ == "__main__":
-    test_get_data(root="..")
+    pass
--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,15 @@
 from model.get_model import get_model
 from utils.parser import get_cfg
+from dataset.cifar100 import get_data, get_classes
 
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau, MultiStepLR
+import torchvision
 from torch import nn
 import torch
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 import unittest
 
@@ -29,3 +32,33 @@ class Tests(unittest.TestCase):
         plt.plot(range(1000), lrs)
         plt.show()
         plt.close('all')
+
+    def test_get_data(self, root=r"..", batch_size=4):
+        def show_batch(loader):
+            plt.close('all')
+            # get some random training images
+            dataiter = iter(loader)
+            images, labels = dataiter.next()
+            print(f"image shape: {images.shape}")
+            print(f"labels shape: {labels.shape}")
+
+            # print labels
+            print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+            # show images
+            imshow(torchvision.utils.make_grid(images))
+
+        train_dataset, train_loader = get_data(root, train=True, batch_size=batch_size)
+        test_dataset, test_loader = get_data(root, train=False, batch_size=batch_size)
+        classes = get_classes(train_dataset)
+
+        print("show train loader")
+        show_batch(train_loader)
+        print("show test loader")
+        show_batch(test_loader)
+
+
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
