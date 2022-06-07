5ee23607d8f98249e0be262e90e009a8c628fa6a
guardianwang <962247283@qq.com>
2022-06-07 05:24:29 -0400

    random perspective

--- dataset/cifar100.py
+++ dataset/cifar100.py
@@ -14,6 +14,7 @@ def get_data(root=r".", train=True, batch_size=4, augment_train=True, extra_augm
         if extra_augment:
             transform.extend([
                 transforms.ColorJitter(0.5, 0.5, 0.5, 0.5),
+                transforms.RandomPerspective(),
                 transforms.RandomGrayscale(),
             ])
         transform.extend([
