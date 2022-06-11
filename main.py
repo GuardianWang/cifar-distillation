b79c0c38592886b7a5692a8309230da414d6146c
guardianwang <962247283@qq.com>
2022-06-11 03:39:09 -0700

    adamw, cosine

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from model.get_model import get_model
 from dataset.cifar100 import get_data
+from optimizer import get_optimizer, get_scheduler
 from utils.parser import get_cfg, cfg_to_str
 
-import torch.optim as optim
-from torch.optim.lr_scheduler import ReduceLROnPlateau, MultiStepLR
 from torch import nn
 import torch
 import torch.nn.functional as F
@@ -100,9 +99,8 @@ def train(cfg):
 
     model = get_model(cfg, cfg.model).to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=cfg.lr, momentum=cfg.momentum,
-                          weight_decay=cfg.weight_decay, nesterov=cfg.nesterov)
-    scheduler = MultiStepLR(optimizer, milestones=cfg.milestones, gamma=cfg.gamma)
+    optimizer = get_optimizer(cfg, model)
+    scheduler = get_scheduler(optimizer, cfg)
     train_dataset, train_loader = get_data(root=cfg.data_root, train=True,
                                            batch_size=cfg.train_batch_size, extra_augment=cfg.extra_augment)
     test_dataset, test_loader = get_data(root=cfg.data_root, train=False, batch_size=cfg.test_batch_size)
@@ -110,13 +108,16 @@ def train(cfg):
 
     for epoch in range(cfg.train_epoch):
         logging.info(f"===epoch {epoch:04d}===")
-        logging.info(f"lr: {scheduler.get_last_lr()}")
+        logging.info(f"lr: {scheduler.get_lr()}")
         train_loss = train_step(model, criterion, optimizer, train_loader, device=device, cfg=cfg)
         if epoch % cfg.test_epoch_freq == 0:
             test_step(model, criterion, test_loader, device=device, cfg=cfg)
         if epoch > 0 and epoch % cfg.save_model_freq == 0:
             torch.save(model.state_dict(), cfg.model_path)
         scheduler.step()
+
+    logging.info(f"===evaluate on training set===")
+    test_step(model, criterion, train_loader, device=device, cfg=cfg)
 
 
 def distill(cfg):
@@ -136,9 +137,8 @@ def distill(cfg):
 
     hard_criterion = nn.CrossEntropyLoss()
     soft_criterion = nn.KLDivLoss()
-    optimizer = optim.SGD(student.parameters(), lr=cfg.lr, momentum=cfg.momentum,
-                          weight_decay=cfg.weight_decay, nesterov=cfg.nesterov)
-    scheduler = MultiStepLR(optimizer, milestones=cfg.milestones, gamma=cfg.gamma)
+    optimizer = get_optimizer(cfg, student)
+    scheduler = get_scheduler(optimizer, cfg)
     train_dataset, train_loader = get_data(root=cfg.data_root, train=True,
                                            batch_size=cfg.train_batch_size, extra_augment=cfg.extra_augment)
     test_dataset, test_loader = get_data(root=cfg.data_root, train=False, batch_size=cfg.test_batch_size)
@@ -149,7 +149,7 @@ def distill(cfg):
 
     for epoch in range(cfg.train_epoch):
         logging.info(f"===epoch {epoch:04d}===")
-        logging.info(f"lr: {scheduler.get_last_lr()}")
+        logging.info(f"lr: {scheduler.get_lr()}")
         train_loss = distill_step(teacher, student, hard_criterion, soft_criterion,
                                   optimizer, train_loader, device, cfg)
         if epoch % cfg.test_epoch_freq == 0:
@@ -158,9 +158,12 @@ def distill(cfg):
             torch.save(student.state_dict(), cfg.model_path)
         scheduler.step()
 
+    logging.info(f"===evaluate on training set===")
+    test_step(student, hard_criterion, train_loader, device=device, cfg=cfg)
+
 
 def test(cfg):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if cfg.not_use_gpu:
         device = torch.device("cpu")
     logging.info(f"device: {device}")
