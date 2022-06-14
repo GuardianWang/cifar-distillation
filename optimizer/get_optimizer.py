863e538ec795124ee9939e6472d3b9b36104fd1e
guardianwang <962247283@qq.com>
2022-06-14 01:35:40 -0700

    by name

--- optimizer/get_optimizer.py
+++ optimizer/get_optimizer.py
@@ -2,6 +2,16 @@ import torch.optim as optim
 
 
 def get_optimizer(cfg, model):
-    opt = optim.AdamW(model.parameters(), lr=cfg.lr, betas=(cfg.beta1, cfg.beta2),
-                      weight_decay=cfg.weight_decay)
+    name = cfg.optimizer
+    if name == "adamw":
+        opt = optim.AdamW(model.parameters(), lr=cfg.lr, betas=(cfg.beta1, cfg.beta2),
+                          weight_decay=cfg.weight_decay, amsgrad=cfg.amsgrad)
+    elif name == "adam":
+        opt = optim.Adam(model.parameters(), lr=cfg.lr, betas=(cfg.beta1, cfg.beta2),
+                         weight_decay=cfg.weight_decay)
+    elif name == "sgd":
+        opt = optim.SGD(model.parameters(), lr=cfg.lr, momentum=cfg.momentum,
+                        weight_decay=cfg.weight_decay, nesterov=cfg.nesterov)
+    else:
+        raise ValueError(f"optimizer {name} not defined")
     return opt
