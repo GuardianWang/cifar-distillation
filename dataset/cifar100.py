8135fd0148b93259d3f750e7fc191a321ef2e7d0
guardianwang <962247283@qq.com>
2022-06-19 04:13:28 -0700

    dataset aug

--- dataset/cifar100.py
+++ dataset/cifar100.py
@@ -8,14 +8,40 @@ from torchtoolbox.transform import Cutout
 from multiprocessing import cpu_count
 
 
-def get_data(root=r".", train=True, batch_size=4, augment_train=True, extra_augment=False):
+def get_data(root=r".", train=True, batch_size=4, augment_train=True, extra_augment=False,
+             cfg=None):
     transform = []
     if train and augment_train:
         if extra_augment:
-            transform.extend([
-                transforms.ColorJitter(0.5, 0.5, 0.5, 0.5),
-                transforms.RandomGrayscale(),
-            ])
+            if cfg:
+                if cfg.ColorJitter:
+                    transform.append(transforms.ColorJitter(
+                        brightness=cfg.brightness,
+                        contrast=cfg.contrast,
+                        saturation=cfg.saturation,
+                        hue=cfg.hue,
+                    ))
+                if cfg.RandomAffine:
+                    transform.append(transforms.RandomAffine(
+                        degrees=cfg.degrees,
+                        translate=(0, cfg.translate_M),
+                        scale=(cfg.scale_m, cfg.scale_M),
+                        shear=cfg.shear,
+                    ))
+                if cfg.RandomPerspective:
+                    transform.append(transforms.RandomPerspective(
+                        distortion_scale=cfg.distortion_scale,
+                        p=cfg.perspective_p,
+                    ))
+                if cfg.RandomGrayscale:
+                    transform.append(transforms.RandomGrayscale(
+                        p=cfg.gray_p
+                    ))
+            else:
+                transform.extend([
+                    transforms.ColorJitter(0.5, 0.5, 0.5, 0.5),
+                    transforms.RandomGrayscale(),
+                ])
         transform.extend([
             transforms.RandomCrop(32, padding=4),
             Cutout(),
